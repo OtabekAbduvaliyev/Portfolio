9964f20a70fd1168ce314a92811cfb9f87402caf
Your Name <you@example.com>
2023-11-25 14:15:31 +0500

    ztp

---
+++ script.js
@@ -0,0 +1,25 @@
+let active = document.querySelector('ul');
+let btn1 = document.querySelector('#nav-btn')
+let a = document.querySelector('#a')
+btn1.addEventListener('click',()=>{
+    active.classList.toggle('active')
+})
+let btn = document.getElementById('sun')
+btn.addEventListener('click',()=>{
+    let mood = document.getElementById('mood');
+    btn.classList.toggle('fa-moon')
+    if(btn.classList.toggle("fa-sun")){
+        mood.href = './style.css';
+    }
+    else{
+        mood.href = './white.css'
+    }
+})
+// a.addEventListener('click',()=>{
+//     // for(let i =0; i < active.length; i++){
+//     //     active[i].classList.toggle('active')
+//     // }
+//     // active.forEach(()=>{
+//     //     active.classList.toggle('active')
+//     // })
+// })
